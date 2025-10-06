df7c5d262d06502688776faa01bfc23eaf5dee4c
Ramon Tenacious <ramontenacious@gmail.com>
2025-10-06 17:00:40 -0300

    HTML ready.

--- frontend/src/components/MainPage/PortalGallery/PortalGallery.jsx
+++ frontend/src/components/MainPage/PortalGallery/PortalGallery.jsx
@@ -2,8 +2,24 @@ import './PortalGallery.css';
 
 export const PortalGallery = () => {
     return (
-        <section id='portalGallery'>
-            <h1 id='portalGallery'>Portal Gallery</h1>
+        <section className='portalGallery' id='portalGallery'>
+            <h2 className='portalGallery__title'>Portal Gallery</h2>
+            <div>
+                <h4>Professional Experiences</h4>
+                <h4>Personal Projects</h4>
+            </div>
+            <div className='portalGallery__content'>
+                <h1>Quasar</h1>
+                <h5>2024 - Present quasarcorp.com</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla metus, tincidunt vitae risus id, rutrum posuere nisl. 
+                    Nam mollis cursus massa, vitae rhoncus neque aliquam a. Ut finibus massa nisi, sed sollicitudin elit elementum ut.
+                </p>
+                <section className='portalGallery__tags'>
+                    <div className='portalGallery__tagsWrapper'>
+                        <h5>Frontend</h5>
+                    </div>
+                </section>
+            </div>
         </section>
     )
 }
