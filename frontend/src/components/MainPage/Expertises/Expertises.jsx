80f1ec5a4abb9c2238f07879e3fd098e8a6a3afe
Ramon Tenacious <ramontenacious@gmail.com>
2025-10-06 14:31:07 -0300

    Expertises sections structure done, still need to add responsiveness.

--- frontend/src/components/MainPage/Expertises/Expertises.jsx
+++ frontend/src/components/MainPage/Expertises/Expertises.jsx
@@ -1,9 +1,37 @@
 import './Expertises.css'
 
 export const Expertises = () => {
+    const options = [
+        {
+            title: "Frontend",
+            content: "React, Next, Javascript, HTML, CSS"
+        },
+        {
+            title: "Backend",
+            content: "Python, Django"
+        },
+        {
+            title: "Others",
+            content: "AWS, Figma"
+        }
+    ]
+
     return (
-        <section id='expertises'>
-            <h1>Expertises</h1>
+        <section id='expertises' className='expertises'>
+            <header className='expertises__header'>
+                <h2 className='expertises__title'>Expertises</h2>
+                <h6 className='expertises__subtitle'>Started my journey at 16 years old...</h6>
+            </header>
+            <div className='expertises__contentWrapper'>
+            {
+                options.map((data, index) => (
+                    <div key={index} className='expertises__content'>
+                        <h3>{data.title}</h3>
+                        <p>{data.content}</p>
+                    </div>
+                ))
+            }
+            </div>
         </section>
     )
 }
