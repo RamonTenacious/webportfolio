d03c5175b9db171beb791d5c2ff341dc3e3c063c
Ramon Tenacious <ramontenacious@gmail.com>
2025-10-08 15:13:15 -0300

    Portifolio full structure done, only remains to add more projects and add animations.

--- frontend/src/components/MainPage/Footer/Footer.jsx
+++ frontend/src/components/MainPage/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import './Footer.css';
+import githubIcon from '../../../assets/images/icons/Github.png';
+import linkedinIcon from '../../../assets/images/icons/Linkedin.png';
 
 export const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <section id='footer'>
-            <h1>Footer</h1>
+        <section className='footer'>
+            <h6>© {currentYear} Tenacious. All rights reserved.</h6>
+            <h6>ramontenacious@gmail.com</h6>
+            <figure className='footer__figure'>
+                <a href='https://github.com/RamonTenacious' target='_blank' rel='noopener noreferrer'>
+                    <img src={githubIcon} alt='Github Icon'/>
+                </a>
+                <a href='https://www.linkedin.com/in/ramon-mauricio-1b0b8829b/' target='_blank' rel='noopener noreferrer'>
+                    <img src={linkedinIcon} alt='Linkedin'/>
+                </a>
+            </figure>
         </section>
-    )
-}
+    );
+};
