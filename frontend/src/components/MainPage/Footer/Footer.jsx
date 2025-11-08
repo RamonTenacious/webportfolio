710a6434852b176bf0df3fbddfaad92107797117
Ramon <ramonmauriciodev@gmail.com>
2025-11-08 13:57:58 -0300

    Footer adjustments.

--- frontend/src/components/MainPage/Footer/Footer.jsx
+++ frontend/src/components/MainPage/Footer/Footer.jsx
@@ -1,21 +1,38 @@
 import './Footer.css';
 import githubIcon from '../../../assets/images/icons/Github.png';
 import linkedinIcon from '../../../assets/images/icons/Linkedin.png';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
     const currentYear = new Date().getFullYear();
+
+    const icons = [
+        {
+            icon: FaGithub,
+            altIcon: "GithubIcon",
+            link: "https://github.com/RamonTenacious"
+        },
+        {
+            icon: FaLinkedin,
+            altIcon: "Linkedin Icon",
+            link: "https://www.linkedin.com/in/ramon-mauricio-1b0b8829b/"
+        }
+    ]
 
     return (
         <section className='footer'>
             <h6>© {currentYear} Tenacious. All rights reserved.</h6>
             <h6>ramontenacious@gmail.com</h6>
             <figure className='footer__figure'>
-                <a href='https://github.com/RamonTenacious' target='_blank' rel='noopener noreferrer'>
-                    <img src={githubIcon} alt='Github Icon'/>
-                </a>
-                <a href='https://www.linkedin.com/in/ramon-mauricio-1b0b8829b/' target='_blank' rel='noopener noreferrer'>
-                    <img src={linkedinIcon} alt='Linkedin'/>
-                </a>
+                {icons.map((data, index) => {
+                    const IconComponent = data.icon;
+
+                    return (
+                        <a key={index} href={data.link} target='_blank' rel='noopener noreferrer'>
+                            <IconComponent className='footer__icon' />
+                        </a>
+                    );
+                })}
             </figure>
         </section>
     );
