379c6e4bce44fcec3db9ef6319640e2377079c09
Ramon <ramonmauriciodev@gmail.com>
2025-11-08 18:49:37 -0300

    Icons added.

--- frontend/src/components/MainPage/Expertises/Expertises.jsx
+++ frontend/src/components/MainPage/Expertises/Expertises.jsx
@@ -1,6 +1,5 @@
 import { EXPERTISES_OPTIONS } from '../../../constants/expertisesOptions'
 import './Expertises.css'
-import React from 'react'
 
 export const Expertises = () => {
     return (
@@ -17,6 +16,11 @@ export const Expertises = () => {
                             <div key={index} className='expertises__content'>
                                 <h3>{data.title}</h3>
                                 <p>{data.content}</p>
+                                <div className='expertises__iconWrapper'>
+                                    { data.icons.map((Icon, index) => (
+                                        <Icon key={index} className='expertises__icon'/>  
+                                    ))}
+                                </div>
                             </div>
                         ))
                     }
--- frontend/src/constants/expertisesOptions.js
+++ frontend/src/constants/expertisesOptions.js
@@ -1,14 +1,34 @@
+import { FaAws, FaCss3Alt, FaFigma, FaHtml5, FaPython, FaReact } from "react-icons/fa";
+import { IoLogoJavascript} from "react-icons/io5";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiDjango } from "react-icons/si";
+
 export const EXPERTISES_OPTIONS = [
     {
         title: "Frontend",
-        content: "React, Next, Javascript, HTML, CSS"
+        content: "React, Next, Javascript, HTML, CSS",
+        icons: [
+            FaHtml5,
+            FaCss3Alt,
+            IoLogoJavascript,
+            FaReact,
+            RiNextjsFill,
+        ]
     },
     {
         title: "Backend",
-        content: "Python, Django"
+        content: "Python, Django",
+        icons: [
+            FaPython,
+            SiDjango,
+        ]
     },
     {
         title: "Others",
-        content: "AWS, Figma"
+        content: "AWS, Figma",
+        icons: [
+            FaAws,
+            FaFigma
+        ]
     }
 ]
