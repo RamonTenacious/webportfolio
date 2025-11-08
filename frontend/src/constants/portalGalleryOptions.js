70602c66b0112d62b71f1dba4fc0198f2d3fc508
Ramon <ramonmauriciodev@gmail.com>
2025-11-08 20:17:33 -0300

    Removed duplicate and placeholder.

--- frontend/src/constants/portalGalleryOptions.js
+++ frontend/src/constants/portalGalleryOptions.js
@@ -19,25 +19,10 @@ export const PORTAL_GALLERY_OPTIONS = [
                     "AWS"
                 ],
                 image: quasarImg
-            },
-            {
-                title: "Quasar",
-                started: 2024,
-                finished: 0,
-                stillPresent: true,
-                website: "quasarcorp.com",
-                paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla metus, tincidunt vitae risus id, rutrum posuere nisl. Nam mollis cursus massa, vitae rhoncus neque aliquam a. Ut finibus massa nisi, sed sollicitudin elit elementum ut.",
-                tags: [
-                    "Frontend",
-                    "Backend",
-                    "React",
-                    "Django",
-                    "AWS"
-                ],
-                image: quasarImg
             }
         ]
     },
+    /*
     {
         title: "Personal Projects",
         content: [
@@ -45,4 +30,5 @@ export const PORTAL_GALLERY_OPTIONS = [
             }
         ]
     }
+    */
 ]
